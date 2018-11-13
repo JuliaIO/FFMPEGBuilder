82e2f31c4ba9aca50c44b8ffd94ba22dd6011a9d
Julian P Samaroo <jpsamaroo@gmail.com>
2018-11-13 14:23:47 -0600

    Added a bunch of dependencies and enabled building with NASM/YASM

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -13,23 +13,38 @@ sources = [
 ]
 
 # Bash recipe for building across all platforms
+# TODO: Theora and Opus once their releases are available
 script = raw"""
 cd $WORKSPACE/srcdir
 cd ffmpeg-4.1/
 sed -i 's/-lflite"/-lflite -lasound"/' configure
 apk add coreutils
-./configure --prefix=$prefix --enable-gpl --enable-version3 --enable-nonfree --disable-static --enable-shared --disable-debug --enable-avresample --disable-x86asm
+./configure            \
+  --prefix=$prefix     \
+  --enable-gpl         \
+  --enable-version3    \
+  --enable-nonfree     \
+  --disable-static     \
+  --enable-shared      \
+  --disable-debug      \
+  --enable-avresample  \
+  --enable-libass      \
+  --enable-libfdk-aac  \
+  --enable-libfreetype \
+  --enable-libmp3lame  \
+  --enable-libvorbis   \
+  --enable-libvpx      \
+  --enable-libx264
+#--enable-libx265
 make -j${nproc}
-$CC tools/qt-faststart.c -o tools/qt-faststart
 make install
-install -v -m755 tools/qt-faststart $prefix/bin
 
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = [
-    Linux(:x86_64, libc=:glibc)
+    Linux(:x86_64, libc=:glibc),
 ]
 
 # The products that we will ensure are always built
@@ -46,8 +61,20 @@ products(prefix) = [
 ]
 
 # Dependencies that must be installed before this package can be built
+# TODO: Theora and Opus once their releases are available
 dependencies = [
-    
+    "https://github.com/JuliaIO/LibassBuilder/releases/download/v0.14.0/build_libass.v0.14.0.jl",
+    "https://github.com/SimonDanisch/FDKBuilder/releases/download/0.1.6/build_libfdk.v0.1.6.jl",
+    "https://github.com/SimonDanisch/NASMBuilder/releases/download/2.13.3/build_nasm.v2.13.3.jl",
+    "https://github.com/SimonDanisch/FribidiBuilder/releases/download/0.14.0/build_fribidi.v0.14.0.jl",
+    "https://github.com/JuliaGraphics/FreeTypeBuilder/releases/download/v2.9.0-0/build.jl",
+    "https://github.com/SimonDanisch/LAMEBuilder/releases/download/3.100.0/build_liblame.v3.100.0.jl",
+    "https://github.com/JuliaIO/LibVorbisBuilder/releases/download/v1.3.6/build_libvorbis.v1.3.6.jl",
+    "https://github.com/staticfloat/OggBuilder/releases/download/v1.3.3-7/build_Ogg.v1.3.3.jl",
+    "https://github.com/jpsamaroo/LibVPXBuilder/releases/download/v5.0.0/build_LibVPX.v5.0.0.jl",
+    "https://github.com/jpsamaroo/YasmBuilder/releases/download/v1.3.0-pre/build_YasmBuilder.v1.3.0.jl",
+    "https://github.com/jpsamaroo/x264Builder/releases/download/v2018.2.12-noyasm/build_x264Builder.v2018.2.12-pre-noyasm.jl",
+    #"https://github.com/jpsamaroo/x265Builder/releases/download/v2.8-noyasm/build_x265Builder.v2.8.0.jl",
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
