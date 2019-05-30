dc79e5dc5aae3759596641f3d919d1c5ed347492
Ian Butterworth <i.r.butterworth@gmail.com>
2019-05-30 18:46:55 -0400

    Dream build - All platforms, all repos logically hosted (#2)
    * Update .travis.yml
    
    * disable deploy
    
    * release any .tar.gz
    
    * catch products
    
    * remove musl
    
    * deploy settings
    
    * enable master
    
    * Attempt (#2)
    
    * attempt to add libx264rgb
    
    * change to --enable-encoder=libx264rgb
    
    * trigger travis on master
    
    * enable all encoders etc
    
    * enable libx264rgb specifically
    
    * hard coded paths for h264
    
    * typo
    
    * fix paths
    
    * change cflags to abs paths
    
    * update x264 to provide gray8 etc.
    
    * going for the perfect setup
    
    * update LAMEBuilder release
    
    * update LibVorbisBuilder release
    
    * update readme
    
    * temporarily disable x265 and libvpx
    
    * cross compile handling
    
    * update secure personal token to mine
    
    * try without docs and skip working platforms
    
    * add sysroot
    
    * fix sysroot with staticfloat's big guns
    
    * update FreeTypeBuilder, disable successful platforms
    
    * update
    
    * x265 pkg-config fix
    
    https://bitbucket.org/multicoreware/x265/issues/371/x265-not-found-using-pkg-config
    
    * fix wins
    
    * Update .travis.yml

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -9,7 +9,6 @@ version = v"4.1.0"
 sources = [
     "https://ffmpeg.org/releases/ffmpeg-4.1.tar.bz2" =>
     "b684fb43244a5c4caae652af9022ed5d85ce15210835bce054a33fb26033a1a5",
-
 ]
 
 # Bash recipe for building across all platforms
@@ -19,23 +18,69 @@ cd $WORKSPACE/srcdir
 cd ffmpeg-4.1/
 sed -i 's/-lflite"/-lflite -lasound"/' configure
 apk add coreutils
+apk add yasm
+if [[ "${target}" == *-linux-* ]]; then
+    export ccOS="linux"
+elif [[ "${target}" == *-apple-* ]]; then
+    export ccOS="darwin"
+elif [[ "${target}" == *-w32-* ]]; then
+    export ccOS="mingw32"
+elif [[ "${target}" == *-w64-* ]]; then
+    export ccOS="mingw64"
+elif [[ "${target}" == *-unknown-freebsd* ]]; then
+    export ccOS="freebsd"
+else
+    export ccOS="linux"
+fi
+if [[ "${target}" == x86_64-* ]]; then
+    export ccARCH="x86_64"
+elif [[ "${target}" == i686-* ]]; then
+    export ccARCH="i686"
+elif [[ "${target}" == arm-* ]]; then
+    export ccARCH="arm"
+elif [[ "${target}" == aarch64-* ]]; then
+    export ccARCH="aarch64"
+elif [[ "${target}" == powerpc64le-* ]]; then
+    export ccARCH="powerpc64le"
+else
+    export ccARCH="x86_64"
+fi
+export PKG_CONFIG_PATH="${prefix}/lib/pkgconfig"
+pkg-config --list-all
 ./configure            \
+  --enable-cross-compile \
+  --cross-prefix=/opt/${target}/bin/${target}- \
+  --arch=${ccARCH}     \
+  --target-os=${ccOS}  \
+  --sysinclude=${prefix}/include \
+  --pkg-config=$(which pkg-config) \
+  --pkg-config-flags=--static \
   --prefix=$prefix     \
+  --sysroot=/opt/${target}/${target}/sys-root \
+  --extra-libs=-lpthread \
   --enable-gpl         \
   --enable-version3    \
   --enable-nonfree     \
   --disable-static     \
   --enable-shared      \
   --disable-debug      \
+  --disable-doc        \
   --enable-avresample  \
   --enable-libass      \
   --enable-libfdk-aac  \
   --enable-libfreetype \
   --enable-libmp3lame  \
   --enable-libvorbis   \
+  --enable-libx264     \
+  --enable-libx265     \
   --enable-libvpx      \
-  --enable-libx264
-#--enable-libx265
+  --enable-encoders    \
+  --enable-decoders    \
+  --enable-muxers      \
+  --enable-demuxers    \
+  --enable-parsers     \
+  --extra-cflags="-I${prefix}/include" \
+  --extra-ldflags="-L${prefix}/lib"
 make -j${nproc}
 make install
 
@@ -44,39 +89,61 @@ make install
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = [
-    Linux(:x86_64, libc=:glibc),
+        # glibc Linuces
+        Linux(:i686),
+        Linux(:x86_64),
+        Linux(:aarch64),
+        Linux(:armv7l),
+        Linux(:powerpc64le),
+
+        # musl Linuces
+        Linux(:i686, :musl),
+        Linux(:x86_64, :musl),
+        Linux(:aarch64, :musl),
+        Linux(:armv7l, :musl),
+
+        # BSDs
+        MacOS(:x86_64),
+        FreeBSD(:x86_64),
+
+        # Windows
+        Windows(:i686),
+        Windows(:x86_64),
 ]
 
 # The products that we will ensure are always built
 products(prefix) = [
-    LibraryProduct(prefix, "libavformat", Symbol("")),
-    LibraryProduct(prefix, "libavcodec", Symbol("")),
-    LibraryProduct(prefix, "libavutil", Symbol("")),
-    LibraryProduct(prefix, "libpostproc", Symbol("")),
-    LibraryProduct(prefix, "libswresample", Symbol("")),
-    LibraryProduct(prefix, "libavdevice", Symbol("")),
-    LibraryProduct(prefix, "libavresample", Symbol("")),
-    LibraryProduct(prefix, "libavfilter", Symbol("")),
-    LibraryProduct(prefix, "libswscale", Symbol(""))
+    ExecutableProduct(prefix, "ffmpeg", :ffmpeg),
+    ExecutableProduct(prefix, "ffprobe", :ffprobe),
+    ExecutableProduct(prefix, "x264", :x264),
+    ExecutableProduct(prefix, "x265", :x265),
+    LibraryProduct(prefix, "libavformat", :libavformat),
+    LibraryProduct(prefix, "libavcodec", :libavcodec),
+    LibraryProduct(prefix, "libavutil", :libavutil),
+    LibraryProduct(prefix, "libpostproc", :libpostproc),
+    LibraryProduct(prefix, "libswresample", :libswresample),
+    LibraryProduct(prefix, "libavdevice", :libavdevice),
+    LibraryProduct(prefix, "libavresample", :libavresample),
+    LibraryProduct(prefix, "libavfilter", :libavfilter),
+    LibraryProduct(prefix, "libswscale", :libswscale)
 ]
 
 # Dependencies that must be installed before this package can be built
 # TODO: Theora and Opus once their releases are available
 dependencies = [
-    "https://github.com/JuliaIO/LibassBuilder/releases/download/v0.14.0/build_libass.v0.14.0.jl",
+    "https://github.com/JuliaIO/LibassBuilder/releases/download/v0.14.0-2/build_libass.v0.14.0.jl",
     "https://github.com/SimonDanisch/FDKBuilder/releases/download/0.1.6/build_libfdk.v0.1.6.jl",
-    "https://github.com/SimonDanisch/NASMBuilder/releases/download/2.13.3/build_nasm.v2.13.3.jl",
     "https://github.com/SimonDanisch/FribidiBuilder/releases/download/0.14.0/build_fribidi.v0.14.0.jl",
-    "https://github.com/JuliaGraphics/FreeTypeBuilder/releases/download/v2.9.0-0/build.jl",
-    "https://github.com/SimonDanisch/LAMEBuilder/releases/download/3.100.0/build_liblame.v3.100.0.jl",
-    "https://github.com/JuliaIO/LibVorbisBuilder/releases/download/v1.3.6/build_libvorbis.v1.3.6.jl",
-    "https://github.com/staticfloat/OggBuilder/releases/download/v1.3.3-7/build_Ogg.v1.3.3.jl",
-    "https://github.com/jpsamaroo/LibVPXBuilder/releases/download/v5.0.0/build_LibVPX.v5.0.0.jl",
-    "https://github.com/jpsamaroo/YasmBuilder/releases/download/v1.3.0-pre/build_YasmBuilder.v1.3.0.jl",
-    "https://github.com/ianshmean/x264Builder/releases/download/v2019.5.25-noyasm/build_x264Builder.v2019.5.25-noyasm.jl",
-    #"https://github.com/jpsamaroo/x265Builder/releases/download/v2.8-noyasm/build_x265Builder.v2.8.0.jl",
+    "https://github.com/JuliaGraphics/FreeTypeBuilder/releases/download/v2.9.1-4/build_FreeType2.v2.10.0.jl",
+    "https://github.com/JuliaIO/LAMEBuilder/releases/download/v3.100.0-2/build_liblame.v3.100.0.jl",
+    "https://github.com/JuliaIO/LibVorbisBuilder/releases/download/v1.3.6-2/build_libvorbis.v1.3.6.jl",
+    "https://github.com/JuliaIO/OggBuilder/releases/download/v1.3.3-7/build_Ogg.v1.3.3.jl",
+    "https://github.com/JuliaIO/LibVPXBuilder/releases/download/v1.8.0/build_LibVPX.v1.8.0.jl",
+    "https://github.com/JuliaIO/x264Builder/releases/download/v2019.5.25/build_x264Builder.v2019.5.25.jl",
+    "https://github.com/JuliaIO/x265Builder/releases/download/v3.0.0-static/build_x265Builder.v3.0.0.jl",
+    "https://github.com/JuliaPackaging/Yggdrasil/releases/download/Bzip2-v1.0.6-0/build_Bzip2.v1.0.6.jl",
+    "https://github.com/ianshmean/ZlibBuilder/releases/download/v1.2.11/build_Zlib.v1.2.11.jl"
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
-
